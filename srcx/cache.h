4a30e599908aebedfab235127966e5e665463785
manyids2 <arun.mukundan@gmail.com>
2023-07-15 14:54:33 +0200

    Basic structure

---
+++ srcx/cache.h
@@ -0,0 +1,44 @@
+#include <stdint.h>
+
+// Strucure of buffer
+#define LAYERS 3
+
+// limit view to 3072x2048, define landscape?
+#define TILE_SIZE 256
+#define MAX_I 12
+#define MAX_J 8
+
+// limit similar to kitty limits (320MB)
+#define MAX_BUFFERS_PER_LAYER 96 * 2
+#define MAX_BUFFERS 96 * 6
+
+typedef struct LayerCache {
+  // Level
+  int level;
+
+  // Center in level, world
+  int64_t sx, sy, wx, wy;
+
+  // slide level tile indices
+  int left, right, top, bottom;
+
+  // buffers
+  uint32_t *bufs[MAX_BUFFERS_PER_LAYER];
+
+  // Level, si, sj of tiles in buffers;
+  int *ll[MAX_BUFFERS_PER_LAYER];
+  int *si[MAX_BUFFERS_PER_LAYER];
+  int *sj[MAX_BUFFERS_PER_LAYER];
+
+  // Kitty id -> will use for locks
+  // NOTE: kid[..] == -1  => not loaded
+  //       kid[..] ==  0  => loading
+  //       kid[..] >=  1  => loaded
+  int *kid[MAX_BUFFERS_PER_LAYER];
+} LayerCache;
+
+typedef struct Cache {
+  // Buffers for each level
+  int levels[LAYERS]; // Layer holds which level
+  LayerCache *layers[LAYERS];
+} Cache;
---
+++ srcx/view.h
@@ -0,0 +1,29 @@
+#include <stdint.h>
+
+typedef struct View {
+  // wx, wy -> world level coords ( slide at max zoom )
+  // l -> level (below are dependent on level)
+  //
+  //   View related
+  //   vmi, vmj -> max tile coords
+  //
+  //   Slide related
+  //   sx, sy -> coords
+  //   si, sj -> tile coords ( row, col )
+  //   smi, smj -> max tile coords
+  int dirty; // only render if dirty
+
+  // Constants upto resize
+  int64_t ww, wh; // in pixels
+  int cols, rows; // term size
+  int cw, ch;     // cell dims
+  int ts;         // tile size
+
+  // Main state ( which should change on keypress )
+  int l, si, sj;    // current level and slide row, col
+  int ol, osi, osj; // old level and slide row, col
+
+  // Computed state
+  int64_t wx, wy, sx, sy; // center positions in pixels
+  int vmi, vmj, smi, smj; // view and slide maximums
+} View;
